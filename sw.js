513b160b745298693b35eab23be03f2020affc1e
seanscairns <seanscairns@gmail.com>
2026-01-19 19:57:19 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Bump this string whenever you want iOS/Safari to fetch fresh assets
+const CACHE = "hockey-tracker-v9";
+
+// Core assets for offline/PWA usage
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
